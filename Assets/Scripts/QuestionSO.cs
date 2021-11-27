23c360aaa30bb1452fe47cb118d9c8005453ad4b
Charith <crathnaya@wiley.com>
2021-11-27 12:05:34 +0530

    Added Scriptable Objects for Questions

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Questions/New Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "New Question Title";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] [Range(0, 3)] int correctAnswerIndex = 0;
+
+    public string GetQuestion() => this.question;
+
+    public int GetGetCorrectAnswerIndex() => this.correctAnswerIndex;
+
+    public string GetAnswerByIndex(int index) => index > 0 && index < answers.Length ? answers[index] : null;
+}
